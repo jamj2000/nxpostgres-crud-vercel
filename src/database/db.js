8bf539116d2ac1c518b5865b04835170f020f1e7
José A. Muñoz Jiménez <jamj2000@gmail.com>
2024-07-24 19:22:54 +0200

    ...

--- src/database/db.js
+++ src/database/db.js
@@ -1,20 +1,38 @@
-import { sql } from '@vercel/postgres';
+// import { sql } from '@vercel/postgres';
+import postgres from 'postgres'
+
+const sql = postgres(process.env.POSTGRES_URL)
+
+// export default sql
+
 
 async function crearTabla() {
     try {
-        const result = await sql`
-        CREATE TABLE IF NOT EXISTS articulos (
+        const result1 = await sql`
+        CREATE TABLE IF NOT EXISTS articulos2 (
             id SERIAL PRIMARY KEY,
             nombre TEXT NOT NULL,
             descripcion TEXT,
             precio DECIMAL(10,2),
-            createdAt TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-        );
+            "createdAt" TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+            "updatedAt" TIMESTAMP DEFAULT CURRENT_TIMESTAMP  
+            );
         `;
-        console.log(result);
+
+        const result2 = await sql`
+        INSERT INTO articulos (nombre, descripcion, precio)
+        VALUES 
+           ('PC', 'Ordenador de sobremesa', 999.99),
+           ('Impresora', 'Impresora Epson', 55.99),
+           ('Teclado', 'Teclado USB', 19.91);
+        `;
+
+        await sql.end()
+        
     } catch (error) {
         console.log(error);
     }
 }
 
 crearTabla();
+
